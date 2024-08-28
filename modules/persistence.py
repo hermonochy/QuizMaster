ca6945682b5c785083a83befcd95fc3d564bbc20
fcrt <you@example.com>
2024-08-28 12:02:01 +1200

    tidy up

--- modules/persistence.py
+++ modules/persistence.py
@@ -13,39 +13,3 @@ class QuizQuestion:
    def __repr__(self):
       return self.question
      
-
-def load_questions_from_json():
-    try:
-        with open('quiz_questions.json', 'r') as file:
-            questions = json.load(file)
-    except FileNotFoundError:
-        return []
-    return questions
-    
-class NoQuizTitleException(Exception):
-   ...    
-    
-def load_quiz(jsonfilename):
-    try:
-        with open(jsonfilename , 'r') as file:
-            quizdata = json.load(file)
-            if not 'title' in quizdata.keys():
-                raise NoQuizTitleException
-    except FileNotFoundError:
-        return None
-    return quizdata
-    
-
-
-def save_questions_json_file(quiz_name: str, quiz_data):
-    try:
-        with open(f'{quiz_name}.json', 'w') as file:
-            json.dump(quiz_data, file)
-        print(f"Saved quiz data to {quiz_name}.json")
-    except Exception as e:
-        print(f"Error saving quiz data: {e}")  
-        
-def save_questions_to_json(questions):
-    with open('quiz_questions.json', 'w') as file:
-        json.dump(questions, file)
-
