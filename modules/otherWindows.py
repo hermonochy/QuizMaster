0e560299cee918986177f5aa0367dcd139ab157b
hermonochy <hermonochy@users.noreply.github.com>
2025-03-15 18:46:07 +0000

    Update preferences and About QuizMaster windows

--- modules/otherWindows.py
+++ modules/otherWindows.py
@@ -7,11 +7,13 @@ from modules.elements import *
 
 def about(BACKGROUND_COLOUR, BUTTON_COLOUR, BLACK):
 
-    description = """
+    description_p1 = """
      QuizMaster is an engaging, interactive game designed to challenge your knowledge and cognitive skills across a variety of topics. Whether you're a trivia buff or simply looking to test your memory, QuizMaster offers a platform to sharpen your intellect while having fun. The game's structure incorporates different modes, allowing players to select their preferred play style, ensuring each session is both enjoyable and educational.
-
+    """
+    description_p2 = """
     QuizMaster features a variety of game modes tailored for different levels of difficulty and gameplay preferences. From the classic countdown quiz to speed runs that challenge your quick thinking, each mode is designed to provide a unique experience. Players can also enjoy a preferences window that allows customization of song choices, volume, and background colors, making the game truly your own.
-
+    """
+    description_p3 = """
     Join the QuizMaster community today, and contribute to a growing repository of quizzes while testing your knowledge in an entertaining format. Remember, learning can be a fun and exciting journey with QuizMaster!
     """
 
@@ -21,11 +23,13 @@ def about(BACKGROUND_COLOUR, BUTTON_COLOUR, BLACK):
         screen.fill(BACKGROUND_COLOUR)
         button_license = Button("Licenses", (SCREEN_WIDTH // 2 - 150, SCREEN_HEIGHT // 2 + 250), 250, 40)
         button_go_back = Button("Main Menu", (SCREEN_WIDTH // 2 - 150, SCREEN_HEIGHT // 2 + 300), 250, 40)
-        button_website = Button("Our website", (SCREEN_WIDTH // 2 - 125, SCREEN_HEIGHT // 2 + 50), 200, 30)
+        button_website = Button("Our website", (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 + 50), 200, 30)
         display_message("About QuizMaster", SCREEN_HEIGHT // 8, 75, BLACK)
-        display_message(description, SCREEN_HEIGHT // 5, 30, BLACK)
-        display_message("For more information, please vist", SCREEN_HEIGHT // 2, 30, BLACK)
-        button_website.draw(screen, BUTTON_COLOUR)
+        display_message(description_p1, SCREEN_HEIGHT // 5, 30, BLACK)
+        display_message(description_p2, SCREEN_HEIGHT // 3, 30, BLACK)
+        display_message(description_p3, SCREEN_HEIGHT // 2.2, 30, BLACK)
+        display_message("For more information, please vist", SCREEN_HEIGHT // 1.85, 30, BLACK)
+        button_website.draw(screen, BACKGROUND_COLOUR, text_colour=(0,0,255))
         button_license.draw(screen, BUTTON_COLOUR)
         button_go_back.draw(screen, BUTTON_COLOUR)
         pygame.display.update()
@@ -54,8 +58,8 @@ def Licenses(BACKGROUND_COLOUR, BUTTON_COLOUR, BLACK):
         button_GPL = Button("GPL v3", (SCREEN_WIDTH // 2 - 150, SCREEN_HEIGHT // 2 - 50), 250, 40)
         button_CC = Button("Creative Commons", (SCREEN_WIDTH // 2 - 175, SCREEN_HEIGHT // 2), 300, 40)
         button_go_back = Button("Go Back", (SCREEN_WIDTH // 2 - 150, SCREEN_HEIGHT // 2 + 300), 250, 40)
-        button_GPL.draw(screen, BUTTON_COLOUR)
-        button_CC.draw(screen, BUTTON_COLOUR)
+        button_GPL.draw(screen, BACKGROUND_COLOUR, text_colour=(0,0,255))
+        button_CC.draw(screen, BACKGROUND_COLOUR, text_colour=(0,0,255))
         button_go_back.draw(screen, BUTTON_COLOUR)
         pygame.display.update()
         for event in pygame.event.get():
